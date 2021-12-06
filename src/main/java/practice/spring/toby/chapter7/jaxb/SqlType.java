bd1cd6948e776cced071f0522eaf8f67b553fce8
ksh9241 <ksh9241@naver.com>
2021-12-06 22:31:27 +0900

    JAXB 생성 및 언마샬링

---
+++ src/main/java/practice/spring/toby/chapter7/jaxb/SqlType.java
@@ -0,0 +1,62 @@
+//
+// �� ������ JAXB(JavaTM Architecture for XML Binding) ���� ���� 2.2.8-b130911.1802 ������ ���� �����Ǿ����ϴ�. 
+// <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a>�� �����Ͻʽÿ�. 
+// �� ������ �����ϸ� �ҽ� ��Ű���� ���������� �� ���� ������ �սǵ˴ϴ�. 
+// ���� ��¥: 2021.12.06 �ð� 09:56:43 PM KST 
+//
+
+
+package practice.spring.toby.chapter7.jaxb;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlType;
+import javax.xml.bind.annotation.XmlValue;
+
+
+/**
+ * <p>sqlType complex type�� ���� Java Ŭ�����Դϴ�.
+ * 
+ * <p>���� ��Ű�� ������ �� Ŭ������ ���ԵǴ� �ʿ��� �������� �����մϴ�.
+ * 
+ * <pre>
+ * &lt;complexType name="sqlType">
+ *   &lt;simpleContent>
+ *     &lt;extension base="&lt;http://www.w3.org/2001/XMLSchema>string">
+ *       &lt;attribute name="key" use="required" type="{http://www.w3.org/2001/XMLSchema}string" />
+ *     &lt;/extension>
+ *   &lt;/simpleContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "sqlType", propOrder = {
+    "value"
+})
+public class SqlType { // <SQL> 태그 한 개당 SqlType 오브젝트가 하나씩 만들어진다.
+
+    @XmlValue
+    protected String value;
+    @XmlAttribute(name = "key", required = true)
+    protected String key;
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String value) {
+        this.key = value;
+    }
+
+}
---
+++ src/test/java/practice/spring/toby/Chapter7JaxbTest.java
@@ -0,0 +1,39 @@
+package practice.spring.toby;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
+import org.junit.Test;
+
+import practice.spring.toby.chapter7.jaxb.SqlType;
+import practice.spring.toby.chapter7.jaxb.Sqlmap;
+
+public class Chapter7JaxbTest {
+
+	@Test
+	public void readSqlmap() throws JAXBException, IOException {
+		String contextPath = Sqlmap.class.getPackage().getName();
+		JAXBContext context = JAXBContext.newInstance(contextPath);
+		
+		Unmarshaller unmarshaller = context.createUnmarshaller(); // 언마샬러 생성 
+		
+		Sqlmap sqlmap = (Sqlmap) unmarshaller.unmarshal(getClass().getResourceAsStream("sqlmap.xml"));
+		
+		List<SqlType> sqlList = sqlmap.getSql();
+		
+		assertThat(sqlList.size(), is(3));
+		assertThat(sqlList.get(0).getKey(), is("add"));
+		assertThat(sqlList.get(0).getValue(), is("insert"));
+		assertThat(sqlList.get(0).getKey(), is("get"));
+		assertThat(sqlList.get(0).getValue(), is("select"));	
+		assertThat(sqlList.get(0).getKey(), is("delete"));
+		assertThat(sqlList.get(0).getValue(), is("delete"));
+	}
+}
