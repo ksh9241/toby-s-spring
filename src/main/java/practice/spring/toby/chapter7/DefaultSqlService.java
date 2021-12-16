cab9ff00e26c647e71d1c2d6f7930c547baa63a9
ksh9241 <ksh9241@naver.com>
2021-12-16 22:16:34 +0900

    내장형 DB 사용하기

--- src/main/java/practice/spring/toby/chapter7/DefaultSqlService.java
+++ src/main/java/practice/spring/toby/chapter7/DefaultSqlService.java
@@ -5,6 +5,7 @@ public class DefaultSqlService extends BaseSqlService{
 	public DefaultSqlService () {
 		// 생성자에서 디폴트 의존 오브젝트를 직접 만들어서 스스로 DI해준다.
 		setSqlReader (new JaxbXmlSqlReader());
-		setSqlRegistry(new HashMapSqlRegistry());
+//		setSqlRegistry(new HashMapSqlRegistry());
+		setSqlRegistry(new ConcurrentHashMapSqlRegistry());
 	}
 }
